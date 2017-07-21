57b3a815a550ccf731bad177c3d4f4727e9d277f
Andrei <droncheg@users.noreply.github.com>
2017-07-21 22:37:19 +0300

    Update nicki-vendor.mk

--- nicki/nicki-vendor.mk
+++ nicki/nicki-vendor.mk
@@ -74,7 +74,9 @@ PRODUCT_COPY_FILES += \
     vendor/sony/nicki/proprietary/lib/libmorpho_scene_detector.so:system/lib/libmorpho_scene_detector.so \
     vendor/sony/nicki/proprietary/lib/libmorpho_super_resolution.so:system/lib/libmorpho_super_resolution.so \
     vendor/sony/nicki/proprietary/lib/liboemcamera.so:system/lib/liboemcamera.so \
-    vendor/sony/nicki/proprietary/lib/libta.so:system/lib/libta.so
+    vendor/sony/nicki/proprietary/lib/libta.so:system/lib/libta.so \
+    vendor/sony/nicki/proprietary/firmware/htc_7010.fw:system/firmware/htc_7010.fw \
+    vendor/sony/nicki/proprietary/firmware/htc_9271.fw:system/firmware/htc_9271.fw
 
 PRODUCT_PACKAGES += \
     tad_static
@@ -181,7 +183,9 @@ PRODUCT_COPY_FILES += \
     vendor/sony/nicki/proprietary/vendor/lib/libwvdrm_L3.so:system/vendor/lib/libwvdrm_L3.so \
     vendor/sony/nicki/proprietary/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
     vendor/sony/nicki/proprietary/vendor/lib/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so \
-    vendor/sony/nicki/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so
+    vendor/sony/nicki/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
+    vendor/sony/nicki/proprietary/firmware/htc_7010.fw:system/firmware/htc_7010.fw \
+    vendor/sony/nicki/proprietary/firmware/htc_9271.fw:system/firmware/htc_9271.fw
 
 PRODUCT_PACKAGES += \
     TimeService \
